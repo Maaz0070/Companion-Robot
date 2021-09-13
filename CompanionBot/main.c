9e9f0ef8df82680f56bb0e85d98319531a0220c2
Maaz Adil <maazadil786@gmail.com>
2021-09-12 17:39:29 -0700

    Documented some code

--- CompanionBot/main.c
+++ CompanionBot/main.c
@@ -9,6 +9,7 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    //Declare initial value of our variables to be used
     int freq = 0;
     double ambient_level = 250;
     double slow_level = 3500;
@@ -21,8 +22,11 @@ int main(int argc, const char * argv[]) {
     _Bool is_Button_Red_Pressed = 0;
     _Bool is_Button_Green_Pressed = 0;
     
+    //Display on screen
     SetDigitalOutput(10, freq);
     
+    //states are predefined conditions the robot will find itself in
+    //IN state 1 we need to find maximum level of green light, move toward it, and press its button.
     while(state == 1)
     {
         Read_PD();
@@ -34,6 +38,7 @@ int main(int argc, const char * argv[]) {
         
     }
     
+    //IN state 2, after pressing green light, we go to second beacon and press its button
     while(state == 2)
     {
         Read_PD();
@@ -43,6 +48,7 @@ int main(int argc, const char * argv[]) {
         check_Pressed_Button();
     }
     
+    //IN state 3, after pressing both buttons we take the robot out of the arena with the escape button
     while(state == 3)
     {
         escape();
@@ -51,7 +57,7 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-
+//Robot move around in circular motion until it finds the beacon depending on light sensor
 void move(void)
 {
     arm_button = GetDigitalInput(1);
@@ -59,15 +65,18 @@ void move(void)
     steer = error*steer_sensitivity;
     speed = forward_speed;
     
+    //Continue spinning if desired light isn't found
     if(PD_sum < ambient_level)
     {
         speed = 0;
         steer = spin_speed;
     }
+    //slowly travel to beacon if desired light is sensed
     if(PD_sum > slow_level)
     {
         speed = slow_speed;
     }
+    //if beacon is in front and arm is up we stop the Robot
     if(PD_sum > stop_level && arm_button == 0)
     {
         speed = 0;
@@ -80,6 +89,7 @@ void move(void)
     SetMotor(9, temp);
 }
 
+//Checks if button is pressed. If it is not pressed we must keep moving the arm to try
 void check_Pressed_Button(void)
 {
     if(is_Button_Red_Pressed == 1 && state == 1 && PD_sum < ambient_level)
@@ -103,6 +113,7 @@ void check_Pressed_Button(void)
     }
 }
 
+//Lets Robot safely get out of arena without running into anything
 void escape(void)
 {
     StartUltrasonic(3, 6);
@@ -110,6 +121,7 @@ void escape(void)
     SetMotor(2, -60);
     SetMotor(9, 60);
     
+    //If Robot is near an object it will continue to move around
     if(distance < 20)
     {
         SetMotor(2, 60);
@@ -120,6 +132,7 @@ void escape(void)
     }
 }
 
+//Move arm on the robot up or down
 void moveArm(int direction)
 {
     if(direction == 1)
@@ -138,16 +151,20 @@ void moveArm(int direction)
     }
 }
 
+//Tells the arm to go downwards
 void press_button(void)
 {
     arm_button = GetDigitalInput(1);
+    //if arm is up and we are near a beacon we must move arm to turn the beacon off depending on which beacon we are near
     if(arm_button == 0 && PD_sum > stop_level)
     {
+        //for the first beacon we must move the arm down and check if red button is pressed
         if(state == 1)
         {
             moveArm(1);
             is_Button_Red_Pressed = 1;
         }
+        //for the second beacon we must move the arm down and check if green button is pressed
         if(state == 2)
         {
             moveArm(1);
